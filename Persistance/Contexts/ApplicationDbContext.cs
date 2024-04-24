a2dee2e7cde260690db3da52ffc2a2acac99c1b7
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-04-24 11:45:32 -0300

    Se creo el applicationdbcontext en la capa de infraestructura en el proyecto persistance

---
+++ Persistance/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Domain.Common;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IDateTimeService _dateTimeService;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IDateTimeService dateTimeService) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            _dateTimeService = dateTimeService;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            //con esto, cada vez que agregamos o modifiquemos algo, se guardara la fecha automaticamente.
+            foreach(var entry in ChangeTracker.Entries<AuditableBaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.Created = _dateTimeService.NowUTC;
+                        break;
+                    case EntityState.Modified: 
+                        entry.Entity.LastUpdated = _dateTimeService.NowUTC;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
